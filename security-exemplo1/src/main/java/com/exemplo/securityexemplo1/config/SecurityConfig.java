c53e751e8d8edeaca9defe5a7dd42531d3c459d0
thayanenns <thayanenns@gmail.com>
2021-03-17 13:52:31 -0300

    projeto iniciado

--- security-exemplo1/src/main/java/com/exemplo/securityexemplo1/config/SecurityConfig.java
+++ security-exemplo1/src/main/java/com/exemplo/securityexemplo1/config/SecurityConfig.java
@@ -1,2 +1,27 @@
-package com.exemplo.securityexemplo1.config;public class SecurityConfig {
+package com.exemplo.securityexemplo1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/hello/**").hasAnyRole("ADMIN","USER").and().formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("maria").password("{noop}maria123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("joao").password("{noop}joao123").roles("USER");
+
+    }
 }
--- security-exemplo1/src/main/java/com/exemplo/securityexemplo1/controller/HomeController.java
+++ security-exemplo1/src/main/java/com/exemplo/securityexemplo1/controller/HomeController.java
@@ -1,2 +1,14 @@
-package com.exemplo.securityexemplo1.controller;public class HomeController {
+package com.exemplo.securityexemplo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "teste";
+    }
 }
+

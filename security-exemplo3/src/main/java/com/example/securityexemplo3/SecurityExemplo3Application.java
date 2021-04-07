697fc9e9f685ed1520631ac40e044e4c8011ebe0
Thayane Nunes <thayanenns@gmail.com>
2021-04-07 13:31:48 -0300

    Exemplo 3, em andamento

---
+++ security-exemplo3/src/main/java/com/example/securityexemplo3/SecurityExemplo3Application.java
@@ -0,0 +1,13 @@
+package com.example.securityexemplo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExemplo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExemplo3Application.class, args);
+	}
+
+}
---
+++ security-exemplo3/src/test/java/com/example/securityexemplo3/SecurityExemplo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityexemplo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExemplo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

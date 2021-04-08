1d9009355eae97823fa9158a918f24b840f50631
Thayane Nunes <thayanenns@gmail.com>
2021-04-08 12:15:24 -0300

    Exemplo 3, criação de usuario

---
+++ security-exemplo3/src/main/java/com/example/securityexemplo3/Role.java
@@ -0,0 +1,11 @@
+package com.example.securityexemplo3;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    public String getAuthority(){
+        return name();
+    }
+}
---
+++ security-exemplo3/src/test/java/com/example/securityexemplo3/User.java
@@ -0,0 +1,34 @@
+package com.example.securityexemplo3;
+
+import lombok.Data;
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+//    @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<Role> roles;
+}
